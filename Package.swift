1a6d022756b49de295ba046c910b0419f055a256
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-12-21 22:59:43 +0100

    wip

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import Foundation
 import PackageDescription
 
 extension String {
-    static let postgres: Self = "CoenttbPostgres"
+    static let postgres: Self = "Postgres"
 }
 
 extension Target.Dependency {
